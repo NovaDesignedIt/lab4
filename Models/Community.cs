2b3cf1515a2482401f2e220b221d66fe44bbc759
Meech <60023374+NovaDesignedIt@users.noreply.github.com>
2021-11-02 21:07:02 -0400

    Add files via upload

---
+++ Models/Community.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class Community
+    {
+        
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DisplayName("Registration Number")]
+        [Required]
+        public string Id {get;set;}
+
+          
+        [Range(3,50, ErrorMessage = "Value for {0} must be between{1} and {2}.")]
+        [DisplayName("Title")]
+        [Required(ErrorMessage = "A Name must be provided")]
+        public string Title { get; set; }
+        
+        [Required(ErrorMessage = "A decimal number must be provided")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName ="money")]
+        [DisplayName("bugdet")]
+        public decimal Budget { get; set; }
+
+        public virtual ICollection<CommunityMembership> Membership { get; set; }
+
+        
+
+    }
+}
---
+++ Models/CommunityMembership.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class CommunityMembership
+    {
+      
+       
+        public int StudentId { get; set; }
+ 
+        public string CommunityId { get; set; }
+        
+        public virtual Community Community { get; set; }
+        public virtual Student Student { get; set; }
+        
+
+
+    }
+}
